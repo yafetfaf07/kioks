bc2e5798a8e637e2ff9ac0327915afd45ef96e15
Nathan <ngetachew68@gmail.com>
2025-05-29 18:12:24 +0300

    Modified kiokspage and added kioksshopitem

--- lib/pages/user-page/kiokspage.dart
+++ lib/pages/user-page/kiokspage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/widgets/user-widgets/kioksshopitem.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class KioksPage extends StatefulWidget {
   const KioksPage({super.key});
@@ -11,10 +13,26 @@ class _KioksPageState extends State<KioksPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('data'),
+      backgroundColor: Colors.grey.shade50,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade50,
+        title: Text('Shemsu Suk', style: GoogleFonts.poppins(fontWeight: FontWeight.w500, fontSize: 20),),
+        centerTitle: true,
       ),
-    );
+      body: SafeArea(
+        child:  Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          child: GridView.builder(
+            itemCount: 10,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, 
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 20,
+            ),
+            itemBuilder: (context, index) => KioksShopItem(),
+                ),
+        ),
+    )
+  );
   }
 }
