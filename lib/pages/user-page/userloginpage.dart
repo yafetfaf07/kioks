9113d4d56430f2eaa8f957b2111915697a6f42d4
Nathan <ngetachew68@gmail.com>
2025-06-01 20:16:30 +0300

    Modified Signup and login page

--- lib/pages/user-page/userloginpage.dart
+++ lib/pages/user-page/userloginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/user-page/usersignuppage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class UserLoginpage extends StatefulWidget {
@@ -19,123 +20,179 @@ class _UserLoginpageState extends State<UserLoginpage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(top: 40),
-          child: Column(
-            children: [
-              Text(
-                'LogIn',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 32,
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 15),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                Text(
+                  'Log In',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 26,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              Form(
-                key: _loginFormkey,
-                child: Column(
-                  children: [
-                    SizedBox(height: 30),
-                    Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Phone Number',
-                          style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.left,
+                const SizedBox(height: 5),
+                Text(
+                  'Welcome back. You\'ve been missed so much.',
+                  style: GoogleFonts.poppins(
+                    color: Colors.grey.shade800,
+                    fontSize: 13,
+                  ),
+                ),
+                Form(
+                  key: _loginFormkey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 50),
+                      Text(
+                        'Phone Number',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13,
                         ),
+                        textAlign: TextAlign.left,
                       ),
-                    ),
-                    const SizedBox(height: 8),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: TextFormField(
+                      const SizedBox(height: 5),
+                      TextFormField(
                         controller: _phonenumbertext,
                         validator: (value) {
                           return null;
                         },
-
                         decoration: InputDecoration(
-                          hintText: ' Phone no',
-                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 18,
+                            horizontal: 15,
+                          ),
+                          hintText: 'Enter your phone number',
+                          hintStyle: TextStyle(color: Colors.grey.shade600),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.white, width: 1),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 2,
+                            ), // Border when enabled
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.green.shade500,
+                              width: 2,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
                         ),
                         keyboardType: TextInputType.phone,
                       ),
-                    ),
-                    const SizedBox(height: 30),
-                    Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Password',
-                          style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.left,
+                      const SizedBox(height: 25),
+                      Text(
+                        'Password',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13,
                         ),
+                        textAlign: TextAlign.left,
                       ),
-                    ),
-                    const SizedBox(height: 8),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextFormField(
+                      TextFormField(
                         controller: _passwordtext,
                         validator: (value) {
                           return null;
                         },
                         decoration: InputDecoration(
-                          hintText: ' Password',
-                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 18,
+                            horizontal: 15,
+                          ),
+                          hintText: 'Enter your password',
+                          hintStyle: TextStyle(color: Colors.grey.shade600),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.white, width: 1),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 2,
+                            ), // Border when enabled
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.green.shade500,
+                              width: 2,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
                         ),
                         obscureText: true,
                       ),
-                    ),
-                    const SizedBox(height: 40),
-                    FractionallySizedBox(
-                      widthFactor: 0.8,
-
-                      child: ElevatedButton(
-                        
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          alignment: Alignment.center,
-                          padding: EdgeInsets.symmetric(
-                            vertical: 20,
-                            horizontal: 25,
-
+                      const SizedBox(height: 40),
+                      FractionallySizedBox(
+                        widthFactor: 0.99,
+            
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 25,
+                            ),
+                            backgroundColor: Colors.green.shade700,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
-                          backgroundColor: Colors.green.shade700,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                          child: Text(
+                            'Log In',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
-                        child: Text(
-                          'Login',
-                          style: GoogleFonts.poppins(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (builder) => UserSignUppage()),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(2, 2, 3, 4)
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: GoogleFonts.poppins(
+                          color: Colors.blue,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
